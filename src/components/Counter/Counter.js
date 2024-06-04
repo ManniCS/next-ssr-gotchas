4a8cd239568a785eeb72126b96df70b5f68f13ef
Manuele Cavalli-Sforza <manuele.cavallisforza@gmail.com>
2024-06-04 18:25:41 -0400

    Try suppress hydration warning

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,8 +1,16 @@
 'use client';
 import React from 'react';
 
+import Spinner from '../Spinner'
+
 function Counter() {
-  const [count, setCount] = React.useState(0);
+  const [count, setCount] = React.useState(() => { 
+    if (typeof window == 'undefined') { 
+      return 0
+    }
+    return Number(window.localStorage.getItem('saved-count') || 0)
+  });
+  // const [count, setCount] = React.useState(null);
 
   React.useEffect(() => { 
     const savedValue = Number(window.localStorage.getItem('saved-count') || 0)
@@ -10,15 +18,19 @@ function Counter() {
   }, [])
 
   React.useEffect(() => {
-    window.localStorage.setItem('saved-count', count);
+    if (typeof count == 'number')  { 
+      window.localStorage.setItem('saved-count', count);
+    }
   }, [count]);
 
   return (
     <button
+      suppressHydrationWarning
       className="count-btn"
       onClick={() => setCount(count + 1)}
     >
       Count: {count}
+      {/* Count: {typeof count == 'number' ? count : <Spinner/>} */}
     </button>
   );
 }
