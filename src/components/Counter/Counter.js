47a24032b503fb63830afa869c98d5b1da03f88c
Manuele Cavalli-Sforza <manuele.cavallisforza@gmail.com>
2024-06-04 18:25:47 -0400

    Revert "Try suppress hydration warning"
    This reverts commit 4a8cd239568a785eeb72126b96df70b5f68f13ef.

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,16 +1,8 @@
 'use client';
 import React from 'react';
 
-import Spinner from '../Spinner'
-
 function Counter() {
-  const [count, setCount] = React.useState(() => { 
-    if (typeof window == 'undefined') { 
-      return 0
-    }
-    return Number(window.localStorage.getItem('saved-count') || 0)
-  });
-  // const [count, setCount] = React.useState(null);
+  const [count, setCount] = React.useState(0);
 
   React.useEffect(() => { 
     const savedValue = Number(window.localStorage.getItem('saved-count') || 0)
@@ -18,19 +10,15 @@ function Counter() {
   }, [])
 
   React.useEffect(() => {
-    if (typeof count == 'number')  { 
-      window.localStorage.setItem('saved-count', count);
-    }
+    window.localStorage.setItem('saved-count', count);
   }, [count]);
 
   return (
     <button
-      suppressHydrationWarning
       className="count-btn"
       onClick={() => setCount(count + 1)}
     >
       Count: {count}
-      {/* Count: {typeof count == 'number' ? count : <Spinner/>} */}
     </button>
   );
 }
